3afdb4f72622f8bb3024b446fdf77c59c0f5d02a
Cinthia Becher <cinthia_becher@hotmail.com>
2022-11-09 10:48:31 -0300

    New Components: button and dropdown

--- book-shelf-react-app/bookshelf/src/components/Form/index.js
+++ book-shelf-react-app/bookshelf/src/components/Form/index.js
@@ -1,15 +1,34 @@
 import './Form.css'
 import InputText from '../InputText';
+import Dropdown from '../Dropdown';
+import Button from '../Button';
 
 const Form = () => {
+
+    const genres = [
+        'Romance',
+        'Action/Adventure', 
+        'History',
+        'Fantasy',
+        'Horror/Mystery',
+        'Biography',
+        'Self-help',
+        'Economics/Finance' 
+    ]
+
+    const save = (evento) => {
+        evento.preventDefault()
+    }
+
     return (
         <section className='form'>
-            <form>
+            <form onSubmit={save}>
                 <h2>Please, provide book's information</h2>
-                <InputText label="Title" placeholder="Type book title" />
-                <InputText label="Author" placeholder="Type book author's name" />
-                <InputText label="Cover" placeholder="Cover image URL" />
-
+                <InputText required = {true} label="Title" placeholder="Type book title" />
+                <InputText required = {true} label="Author" placeholder="Type book author's name" />
+                <InputText required = {true} label="Cover" placeholder="Cover image URL" />
+                <Dropdown label = "Genre" items = {genres}></Dropdown>
+                <Button>Add new book</Button>
             </form>
         </section>
     )
