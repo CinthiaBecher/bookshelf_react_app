0736369da365a3b65dc5ad9a93aacf95c23c400f
Cinthia Becher <cinthia_becher@hotmail.com>
2022-11-30 17:24:33 -0300

    Genre Section Update

--- book-shelf-react-app/bookshelf/src/App.js
+++ book-shelf-react-app/bookshelf/src/App.js
@@ -4,6 +4,44 @@ import Form from './components/Form';
 import GenreSection from './components/GenreSection';
 
 function App() {
+
+    const genres = [
+        {
+            name: 'Romance',
+            firstColor: '#E896B3',
+            secondColor: '#F6E0E1'
+        },
+        {
+            name: 'Action/Adventure',
+            firstColor: '#B8F1B',
+            secondColor: '#DEF9DB'
+        },
+        {
+            name: 'Fantasy',
+            firstColor: '#BD7AFF',
+            secondColor: '#F5E6FF'
+        },
+        {
+            name: 'Horror/Mystery',
+            firstColor: '#B1B1B1',
+            secondColor: '#E9E9E9'
+        },
+        {
+            name: 'Biography',
+            firstColor: '#F6E0A5',
+            secondColor: '#F8EED4'
+        },
+        {
+            name: 'Self-help',
+            firstColor: '#82CFFA',
+            secondColor: '#DAEEFA'
+        },
+        {
+            name: 'Economics/Finance',
+            firstColor: '#A6D157',
+            secondColor: '#E7F0D1'
+        },
+    ]
     const [books, setBooks] = useState([])
 
     const onNewBookAdded = (book) => {
